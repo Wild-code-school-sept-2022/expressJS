a5123027a126a9309fe1d8dbd65a436f39cc4343
dldvlpr <dormoy.dev@gmail.com>
2022-09-17 00:40:08 +0200

    Base of application movie

--- app.js
+++ app.js
@@ -1,61 +1,26 @@
+require("dotenv").config();
+
 const express = require("express");
 
 const app = express();
 
-const port = 5000;
-
+const port = process.env.APP_PORT ?? 5000;
 
 const welcome = (req, res) => {
-	res.send("Welcome to my favourite movie list");
+  res.send("Welcome to my favourite movie list");
 };
 
 app.get("/", welcome);
 
-const movies =
-	[{
-		id: 1,
-		title: "Citizen Kane",
-		director: "Orson Wells",
-		year: "1941",
-		colors: false,
-		duration: 120,
-	},
-		{
-			id: 2,
-			title: "The Godfather",
-			director: "Francis Ford Coppola",
-			year: "1972",
-			colors: true,
-			duration: 180,
-		},
-		{
-			id: 3,
-			title: "Pulp Fiction",
-			director: "Quentin Tarantino",
-			year: "1994",
-			color: true,
-			duration: 180,
-		},
-	];
+const movieHandlers = require("./movieHandlers");
 
-const getMovies = (req, res) => {
-	res.status(200).send(movies);
-}
-
-app.get("/api/movies", getMovies);
-
-const getMovie = (req, res) => {
-	const movie = movies.find(m => m.id === parseInt(req.params.id));
-	if (!movie) res.status(404).send('not found');
-	res.send(movie);
-}
-
-app.get("/api/movies/:id", getMovie)
+app.get("/api/movies", movieHandlers.getMovies);
+app.get("/api/movies/:id", movieHandlers.getMovieById);
 
 app.listen(port, (err) => {
-	if (err) {
-		console.error("Something bad happened");
-	} else {
-		console.log(`Server is listening on ${port}`);
-	}
+  if (err) {
+    console.error("Something bad happened");
+  } else {
+    console.log(`Server is listening on ${port}`);
+  }
 });
