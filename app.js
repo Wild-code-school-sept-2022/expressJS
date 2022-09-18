06ef5d1657109670e67d448fa2ead1a4a9f1d2b6
dldvlpr <dormoy.dev@gmail.com>
2022-09-18 22:27:46 +0200

    finish 04 bis

--- app.js
+++ app.js
@@ -9,27 +9,31 @@ app.use(express.json());
 const port = process.env.APP_PORT ?? 5000;
 
 const welcome = (req, res) => {
-  res.send("Welcome to my favourite movie list");
+	res.send("Welcome to my favourite movie list");
 };
 
 app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
-const userHandlers = require("./userHandlers")
+const userHandlers = require("./userHandlers");
+const validateMovie = require("./validateMovie");
+const validateUser = require("./validateUser");
 
 app.get("/api/movies", movieHandlers.getMovies);
-app.post("/api/movies", movieHandlers.postMovie);
+app.post("/api/movies", validateMovie.validateMovie, movieHandlers.postMovie);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
-app.put("/api/movies/:id", movieHandlers.putMovie);
+app.put("/api/movies/:id", validateMovie.validateMovie, movieHandlers.putMovie);
 
 app.get("/api/users", userHandlers.getUsers);
-app.post("/api/users", userHandlers.postUser);
+app.post("/api/users", validateUser.validateUser, userHandlers.postUser);
 app.get("/api/users/:id", userHandlers.getUserById);
+app.put("/api/users/:id", validateUser.validateUser, userHandlers.putUser);
+
 
 app.listen(port, (err) => {
-  if (err) {
-    console.error("Something bad happened");
-  } else {
-    console.log(`Server is listening on ${port}`);
-  }
+	if (err) {
+		console.error("Something bad happened");
+	} else {
+		console.log(`Server is listening on ${port}`);
+	}
 });
